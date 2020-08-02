e2ef6526befaa3fabeabec942941c84e2c2cb953
Eduardo Carvajal <jeduardocarvajal@outlook.com>
2020-08-02 13:23:31 -0500

    Added Add method

--- ConsoleAppGIT/ConsoleAppGIT/Feacture1.cs
+++ ConsoleAppGIT/ConsoleAppGIT/Feacture1.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace ConsoleAppGIT
 {
     class Feacture1
     {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
+
     }
 }
